ed1fe48cde7d3926c3680d1c1c2950b2c14abad2
olegpan1 <olegpan@hotmail.com>
2021-04-02 17:48:23 +0700

    2.2. Рефакторинг - Площадь треугольника. [#504735]

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double semiPerimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        double rounding = 0.000001;
+        return (ab + ac > bc + rounding) && (ab + bc > ac + rounding) && (ac + bc > ab + rounding);
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = semiPerimeter(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+
+}
---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.oop;
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TriangleTest {
+
+    @Test
+    public void rectangular() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 3);
+        Point c = new Point(2, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(3, 0.001));
+    }
+
+    @Test
+    public void isosceles() {
+        Point a = new Point(3, 1);
+        Point b = new Point(4, 2);
+        Point c = new Point(5, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(1, 0.001));
+    }
+
+    @Test
+    public void negativeCoordinates() {
+        Point a = new Point(2, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(3, -2);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(2, 0.001));
+    }
+
+    @Test
+    public void nonTriangle() {
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 3);
+        Point c = new Point(4, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(-1, 0.001));
+    }
+
+}
