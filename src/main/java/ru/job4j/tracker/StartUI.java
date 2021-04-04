c8ee35fa4800411ec5ad0c5fc434719ef67bca30
olegpan1 <olegpan@hotmail.com>
2021-04-04 12:10:11 +0700

    6. Date. Отображение даты. [#504753]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI extends Item {
+    public static void main(String[] args) {
+        Item time = new Item();
+        LocalDateTime currentDateTime = time.getTime();
+        System.out.println("Текущие дата и время до форматирования: " + currentDateTime);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = currentDateTime.format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+    }
+}
