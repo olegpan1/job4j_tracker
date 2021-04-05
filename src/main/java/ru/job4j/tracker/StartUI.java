04e1efbac733d2b85b818388800a5fb7e0fe1e5e
olegpan1 <olegpan@hotmail.com>
2021-04-05 15:11:04 +0700

    5. Tracker - хранилище [#504766]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,11 +5,15 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI extends Item {
     public static void main(String[] args) {
-        Item time = new Item();
+        Item time = new Item("Time");
         LocalDateTime currentDateTime = time.getTime();
         System.out.println("Текущие дата и время до форматирования: " + currentDateTime);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = currentDateTime.format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+
+        Tracker tracker = new Tracker();
+        tracker.add(time);
+        tracker.findById(1).printInfo();
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithOutNull = new Item[items.length];
+        int index = 0;
+        for (Item item : items
+        ) {
+            if (item != null) {
+                itemsWithOutNull[index++] = item;
+            }
+        }
+        itemsWithOutNull = Arrays.copyOf(itemsWithOutNull, index);
+        return itemsWithOutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsEqualNames = new Item[items.length];
+        int index = 0;
+        for (Item item : items
+        ) {
+            if (item.getName().equals(key)) {
+                itemsEqualNames[index++] = item;
+            }
+        }
+        itemsEqualNames = Arrays.copyOf(itemsEqualNames, index);
+        return itemsEqualNames;
     }
 
     public Item findById(int id) {
