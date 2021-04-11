565fa458184c0e1ba924af70884cdc4f0e3d9e8d
olegpan1 <olegpan@hotmail.com>
2021-04-11 17:16:39 +0700

    0.6. Error - исключения, связанные с работой виртуальной машины [#504711]

---
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -0,0 +1,13 @@
+package ru.job4j.ex;
+
+public class FactRec {
+    public static int calc(int n) {
+        return (n == 0 || n == 1) ? 1 : calc(n - 1) * n;
+    }
+
+    public static void main(String[] args) {
+        int rsl = calc(4);
+        System.out.println(rsl);
+    }
+
+}
