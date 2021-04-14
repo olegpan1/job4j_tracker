fa50465f16c9697366fa7c227d1319f20025e78a
olegpan1 <olegpan@hotmail.com>
2021-04-14 19:44:40 +0700

    4. final [#504727]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -18,4 +18,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("34");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    @Test
+    public void whenNotFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("532");
+        assertThat(persons.size(), is(0));
+    }
 }
