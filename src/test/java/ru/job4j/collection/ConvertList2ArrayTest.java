ae067c50f1feaff646610e1a9b3646a1f347060c
olegpan1 <olegpan@hotmail.com>
2021-04-14 16:39:09 +0700

    3. Конвертация ArrayList в двухмерный массив [#504662]

--- src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.search;
+package ru.job4j.collection;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 @Ignore
 public class ConvertList2ArrayTest {
