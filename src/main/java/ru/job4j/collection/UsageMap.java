331536960efa7fe5d0a4a1592d2b814e1a81f371
olegpan1 <olegpan@hotmail.com>
2021-04-15 12:00:10 +0700

    0. Map, HashMap [#504667]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("ivanov@yandex.ru", "Ivan Ivanov");
+        map.put("petrov@yandex.ru", "Stepan Petrov");
+        map.put("petrov@yandex.ru", "John Smith");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
