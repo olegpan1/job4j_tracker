1e10778309ab06fca7b1dcddb06edd55973ebaed
olegpan1 <olegpan@hotmail.com>
2021-04-25 15:06:01 +0700

    0. Сортировка [#504657]

--- src/main/java/ru/job4j/tracker/ItemSorterDown.java
+++ src/main/java/ru/job4j/tracker/ItemSorterDown.java
@@ -1,14 +1,8 @@
 package ru.job4j.tracker;
 
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 public class ItemSorterDown implements Comparator<Item> {
-    public static List<Item> sortDownById(List<Item> list) {
-        Collections.sort(list);
-        return list;
-    }
 
     @Override
     public int compare(Item o1, Item o2) {
--- src/main/java/ru/job4j/tracker/ItemSorterUp.java
+++ src/main/java/ru/job4j/tracker/ItemSorterUp.java
@@ -1,14 +1,8 @@
 package ru.job4j.tracker;
 
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 public class ItemSorterUp implements Comparator<Item> {
-    public static List<Item> sortUpByName(List<Item> list) {
-        Collections.sort(list);
-        return list;
-    }
 
     @Override
     public int compare(Item o1, Item o2) {
