f2c56de95f97056239df9fa7d20c9a4bba3f9d79
olegpan1 <olegpan@hotmail.com>
2021-05-16 19:38:16 +0700

    2.2. Лямбда блок [#504484]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,25 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        String[] numbers = {
+                "1. Task",
+                "2. Task",
+                "11. Task",
+                "4. Task"
+        };
+        Comparator<String> numStr = (left, right) -> {
+            int first = Integer.parseInt(left.substring(0, left.indexOf(".")));
+            int second = Integer.parseInt(right.substring(0, right.indexOf(".")));
+            System.out.println("compare - " + second + " : " + first);
+            return Integer.compare(second, first);
+        };
+        Arrays.sort(numbers, numStr);
+        for (String str : numbers) {
+            System.out.println(str);
+        }
+    }
+}
