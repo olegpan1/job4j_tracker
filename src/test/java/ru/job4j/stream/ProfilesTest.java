72732b5756e3a085b0d25a28ef391bd8e525f0bc
olegpan1 <olegpan@hotmail.com>
2021-05-24 16:58:38 +0700

    3. Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -9,18 +9,20 @@ import static org.junit.Assert.assertEquals;
 public class ProfilesTest {
 
     @Test
-    public void collectAddress() {
+    public void collectAddressAndSort() {
         List<Profile> profile = List.of(
                 new Profile(new Address("Moscow", "Red Square", 1, 2)),
                 new Profile(new Address("London", "Baker street", 221, 1)),
+                new Profile(new Address("London", "Baker street", 221, 1)),
+                new Profile(new Address("Moscow", "Red Square", 1, 2)),
                 new Profile(new Address("Berlin", "Under den Linden", 45, 3))
         );
         Profiles pr = new Profiles();
         List<Address> rsl = pr.collect(profile);
         List<Address> expected = List.of(
-                new Address("Moscow", "Red Square", 1, 2),
+                new Address("Berlin", "Under den Linden", 45, 3),
                 new Address("London", "Baker street", 221, 1),
-                new Address("Berlin", "Under den Linden", 45, 3));
+                new Address("Moscow", "Red Square", 1, 2));
         assertEquals(rsl, expected);
     }
 }
