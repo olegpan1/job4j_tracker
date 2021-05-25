1bc64539a8281e4125034b60dc72903426175140
olegpan1 <olegpan@hotmail.com>
2021-05-25 12:01:56 +0700

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 /**
  * Класс описывает работу банка с клиентом
+ *
  * @author OLEG PANYUKOV
  * @version 1.0
  */
@@ -19,6 +20,7 @@ public class BankService {
     /**
      * Добавление клиента.
      * Проверяется наличие клиента в системе
+     *
      * @param user - информация о клиенте
      */
     public void addUser(User user) {
@@ -28,8 +30,9 @@ public class BankService {
     /**
      * Добавление счета клиента.
      * Проверяется наличие такого счета
+     *
      * @param passport - номер паспорта для поиска клиента
-     * @param account - информация о счете
+     * @param account  - информация о счете
      */
     public void addAccount(String passport, Account account) {
         List<Account> findAccount = users.get(findByPassport(passport));
@@ -40,48 +43,46 @@ public class BankService {
 
     /**
      * Поиск клиента по паспорту
+     *
      * @param passport - номер паспорта для поиска клиента
      * @return - возвращает данные клиента, если клиент не найден возвращает null
      */
     public User findByPassport(String passport) {
-        User user = null;
-        for (User find : users.keySet()) {
-            if (find.getPassport().equals(passport)) {
-                user = find;
-                break;
-            }
-        }
-        return user;
+        return users.keySet()
+                .stream()
+                .filter(f -> f.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
      * Поиск счета клиента по реквизитам
-     * @param passport - номер паспорта для поиска клиента
+     *
+     * @param passport  - номер паспорта для поиска клиента
      * @param requisite - реквизиты счета
      * @return - возвращает счет клиента, если счет не найден возвращает null
      */
     public Account findByRequisite(String passport, String requisite) {
-        Account account = null;
         List<Account> findAccount = users.get(findByPassport(passport));
         if (findAccount != null) {
-            for (Account findRequisite : findAccount) {
-                if (findRequisite.getRequisite().equals(requisite)) {
-                    account = findRequisite;
-                    break;
-                }
-            }
+            return findAccount
+                    .stream()
+                    .filter(f -> f.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
-        return account;
+        return null;
     }
 
     /**
      * Переводит деньги с одного счета на другой
      * Если счёта не найдены или не хватает денег на счёте srcAccount возращает false
-     * @param srcPassport - паспорт клиента с счета которого переводят деньги
-     * @param srcRequisite - реквизиты счета с которого переводят деньги
-     * @param destPassport - паспорт клиента на счет которого переводят деньги
+     *
+     * @param srcPassport   - паспорт клиента с счета которого переводят деньги
+     * @param srcRequisite  - реквизиты счета с которого переводят деньги
+     * @param destPassport  - паспорт клиента на счет которого переводят деньги
      * @param destRequisite - реквизиты счета на который переводят деньги
-     * @param amount - размер перевода
+     * @param amount        - размер перевода
      * @return - возвращает true Если перевод произведен
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
