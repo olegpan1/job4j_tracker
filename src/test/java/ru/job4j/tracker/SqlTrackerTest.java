2b9a45dc522814ba60212158cb5c037d241974c1
olegpan1 <olegpan@hotmail.com>
2022-08-23 21:14:26 +0700

    3. Liquibase. Интеграционные тесты. [#504894]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -53,8 +53,7 @@ public class SqlTrackerTest {
     @Test
     public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item"));
         MatcherAssert.assertThat(tracker.findById(item.getId()), is(item));
     }
 
@@ -63,53 +62,36 @@ public class SqlTrackerTest {
         SqlTracker tracker = new SqlTracker(connection);
         Item itemOld = tracker.add(new Item("itemOld"));
         Item itemNew = new Item("itemNew");
-        boolean replaced = tracker.replace(itemOld.getId(), itemNew);
-        assertTrue(replaced);
+        assertTrue(tracker.replace(itemOld.getId(), itemNew));
         MatcherAssert.assertThat(tracker.findById(itemOld.getId()).getName(), is("itemNew"));
     }
 
     @Test
     public void whenDelete() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item1");
-        Item item2 = new Item("item2");
-        Item item3 = new Item("item3");
-        tracker.add(item1);
-        tracker.add(item2);
-        tracker.add(item3);
-        boolean deleted = tracker.delete(item2.getId());
-        assertTrue(deleted);
-        assertNull(tracker.findById(item2.getId()));
+        Item item1 = tracker.add(new Item("item1"));
+        assertTrue(tracker.delete(item1.getId()));
+        assertNull(tracker.findById(item1.getId()));
     }
 
     @Test
     public void whenFindAll() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item1");
-        Item item2 = new Item("item2");
-        Item item3 = new Item("item3");
-        tracker.add(item1);
-        tracker.add(item2);
-        tracker.add(item3);
-        List<Item> result = tracker.findAll();
-        assertEquals(3, result.size());
+        tracker.add(new Item("item1"));
+        tracker.add(new Item("item2"));
+        tracker.add(new Item("item3"));
+        assertEquals(3, tracker.findAll().size());
     }
 
     @Test
     public void whenFindByName() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item1");
-        Item item2 = new Item("item2");
-        Item item3 = new Item("item3");
-        tracker.add(item1);
-        tracker.add(item2);
-        tracker.add(item3);
-        List<Item> result = tracker.findByName("item2");
-        assertEquals(1, result.size());
-        result = tracker.findByName("tem");
-        assertEquals(3, result.size());
-        result = tracker.findByName("qqq");
-        assertEquals(0, result.size());
+        Item item1 = tracker.add(new Item("item1"));
+        Item item2 = tracker.add(new Item("item2"));
+        Item item3 = tracker.add(new Item("item3"));
+        assertEquals(tracker.findByName("item2"), List.of(item2));
+        assertEquals(tracker.findByName("tem"), List.of(item1, item2, item3));
+        assertEquals(tracker.findByName("qqq"), List.of());
 
     }
 
